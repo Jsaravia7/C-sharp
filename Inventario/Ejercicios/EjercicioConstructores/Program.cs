eba1a6b95fb7df614e83a4f750fc94a32f5f5243
Jorge Saravia <jorgesaravia882@gmail.com>
2022-07-13 22:11:26 -0600

    Ej1

---
+++ Inventario/Ejercicios/EjercicioConstructores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Alumno a = new Alumno();
+          a.Id = 1;
+          a.Primernombre = "Juan";
+          a.SegundoNombre = "Perez";
+
+          Alumno b = new Alumno();
+          b.Id = 2;
+          b.Primernombre = "Maria";
+          b.SegundoNombre = "Martinez";
+
+          Alumno c =new Alumno(3);
+          b.Primernombre = "Pedro";
+
+          Console.WriteLine(a.Id);
+          Console.WriteLine(b.Id);
+          Console.WriteLine(c.Id);
+        }
+    }
+}
