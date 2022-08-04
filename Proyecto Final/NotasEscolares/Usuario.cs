6c32aa51df69a189b2e028336d9ab771a8159d6a
Jorge Saravia <jorgesaravia882@gmail.com>
2022-08-04 14:55:16 -0600

    Proyecto final

--- Proyecto Final/NotasEscolares/Usuario.cs
+++ Proyecto Final/NotasEscolares/Usuario.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-public class Usuario: Persona // herencia // 
+public class Usuario: Persona 
 {
    public Usuario(string nombre,string contraseña)
    {
